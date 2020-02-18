07658e09a2dbdc2ed68cf8e6b3186785b7e92d75
JT Liner <jtliner925@gmail.com>
2020-02-17 21:17:26 -0600

    adding the js to the app

---
+++ index.js
@@ -0,0 +1,64 @@
+'use strict';
+
+const STORE = {
+  questions: [
+    {
+      question: 'What did Jesus tell his followers they were supposed to be like?',
+      answers: [
+        '',
+        '',
+        '',
+        'Salt of the earth'
+      ],
+      correctAnswer: 'Salt of the earth'
+    },
+    {
+      question: 'Where did Jesus tell his followers to go?',
+      answers: [
+        'To all nations, preaching the gospel',
+        '',
+        '',
+        ''
+      ],
+      correctAnswer: 'To all nations, preaching the gospel'
+    },
+    {
+      question: 'What did Jesus call himself?',
+      answers: [
+        '',
+        '',
+        'Son of God',
+        ''
+      ],
+      correctAnswer: 'Son of God'
+    },
+    {
+      question: 'What did Jesus call the religious leaders of his day?',
+      answers: [
+        'Snakes and brood of Vipers! ',
+        '',
+        '',
+        ''
+      ],
+      correctAnswer: 'Snakes and brood of Vipers! '
+    },
+    {
+      question: 'What did Jesus call his followers?',
+      answers: [
+        '',
+        '',
+        'Brothers and Sisters',
+        ''
+      ],
+      correctAnswer: 'Brothers and Sisters'
+    },
+  ],
+  questionNumber: 0,
+  score: 0,
+  gotItRight: true,
+  answerUserChose: undefined
+};
+
+function render (){
+    
+}
