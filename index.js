c88590ec1e0127f838154cdf31a901808cf737b1
JT Liner <jtliner925@gmail.com>
2020-02-18 09:02:58 -0600

    moving html to js

--- index.js
+++ index.js
@@ -81,6 +81,43 @@ function correctAnswerValidator(){
 
 //is called to render html for the beginning of the quiz
 function startPage(){
+ return `
+ <body>
+    <section class="start-page">
+    <header>
+        <h1>Jesus said ...WHAT?</h1>
+    </header>
+    
+    <form action="">
+
+        <h2>Do you get the words of Jesus mixed up with other historical figures? <br>Test your knowledge...</h2>
+        <h2>You just might be surprised!</h2>
+       
+    </form>
+    <button name="start" id="start">START</button>
+    </section>
+    <section class="question-page">
+        <header>
+            <h1>Jesus said ...WHAT?</h1>
+        </header>
+    <form action="">
+            
+        <h2>What did Jesus tell his followers they were supposed to be like?</h2>
+        <div class="result-answer">
+        <div class="answers">
+            <button class="ans-choice">Stars in the sky</button><br>
+            <button class="ans-choice">Bread of the meal</button><br>
+            <button class="ans-choice">Salt of the earth</button><br>
+            <button class="ans-choice">Fire in the desert</button><br>
+        </div><br>
+    </div>
+        <button class="submit">SUBMIT</button>
+</form>
+<p class="result-total">
+    You answered 4 out of 6 correctly!
+</p>
+</section>
+</body>`;
 
 }
 //This listens to the start page for clicks to start the quiz.
